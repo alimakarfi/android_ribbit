472610744a6b6875d53e5185befe5ebc169c5ac8
Abdo Daoud <abdo@daoud.co>
2014-10-18 11:40:48 +0300

    Completed relating users in Parse.com

---
+++ app/src/main/java/com/abdodaoud/ribbit/ParseConstants.java
@@ -0,0 +1,13 @@
+package com.abdodaoud.ribbit;
+
+/**
+ * Created by Abdo Daoud on 10/18/14.
+ */
+public final class ParseConstants {
+    // Class name
+
+
+    // Field names
+    public static final String KEY_USERNAME = "username";
+    public static final String KEY_FRIENDS_RELATION = "friendsRelation";
+}
