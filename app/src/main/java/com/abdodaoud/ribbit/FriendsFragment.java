66c18487addc4ba14e63e94f000d38cec64c65f1
Abdo Daoud <abdo@daoud.co>
2014-10-18 09:59:26 +0300

    Completed using Fragments for Tabs

---
+++ app/src/main/java/com/abdodaoud/ribbit/FriendsFragment.java
@@ -0,0 +1,21 @@
+package com.abdodaoud.ribbit;
+
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Abdo Daoud on 10/18/14.
+ */
+
+public class FriendsFragment extends ListFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_friends, container, false);
+        return rootView;
+    }
+}
