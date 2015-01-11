75eeae8e473373d102f651cef0e645b595826faa
Abdo Daoud <abdo.daoud@ubanquity.com>
2015-01-11 21:46:06 +0200

    User can now recieve and view image/video messages.

---
+++ app/src/main/java/com/abdodaoud/ribbit/ViewImageActivity.java
@@ -0,0 +1,37 @@
+package com.abdodaoud.ribbit;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+
+public class ViewImageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_image);
+        // Show the Up Button in the action Bar
+        setupActionBar();
+
+        ImageView imageView = (ImageView)findViewById(R.id.imageView);
+
+        Uri imageUri = getIntent().getData();
+
+        Picasso.with(this).load(imageUri.toString()).into(imageView);
+    }
+
+    /**
+     * Set up the {@link android.app.ActionBar}.
+     */
+    private void setupActionBar() {
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+}
